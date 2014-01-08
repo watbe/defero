0e473942fbaddd72945f70554ff09ac12b93a4cf
Wayne Tsai <wayne@tsai.co>
2014-01-08 16:26:36 +1100

    Initial comment

---
+++ messenger/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Officer(models.Model):
+    user = models.OneToOneField(get_user_model())
+    role = models.CharField(max_length=100)
+
+
+class Message(models.Model):
+    # Editable by user
+    recipients = models.ManyToManyField(Officer)
+    author = models.ForeignKey(get_user_model(), blank=True)
+    content = models.TextField()
+
+    # Hidden from user
+    time_posted = models.DateTimeField()
+    conversation_id = models.IntegerField()
