2d0228d67430c6c59c4097e4bc6655e11ed1e5a6
Wayne Tsai <wayne@tsai.co>
2014-01-08 17:03:57 +1100

    minor admin stuff

--- messenger/admin.py
+++ messenger/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from messenger.models import Officer, Message
 
 # Register your models here.
+admin.site.register(Officer)
+admin.site.register(Message)
---
+++ messenger/urls.py
@@ -0,0 +1,9 @@
+__author__ = 'Wayne'
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+                       # Examples:
+                       # url(r'^$', 'defero.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+                       url(r'^$', 'messenger.views.home'),
+                       )
