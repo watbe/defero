fb568d5d2782c7175c052dd1af5fea3f2b9dd37a
Wayne Tsai <wayne@tsai.co>
2014-01-12 11:36:42 +1100

    adding basic tests for now

--- messenger/tests.py
+++ messenger/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from messenger.models import Officer, BaseMessage, AnonymousMessage, Reply, Conversation
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class OfficerTestCase(TestCase):
+    def setUp(self):
+        user = get_user_model().objects.create(username="tester")
+        Officer.objects.create(user=user, role="Test Officer")
+
+    def test_officer_string(self):
+        user = get_user_model().objects.get(username="tester")
+        officer = Officer.objects.get(user=user)
+        self.assertEqual(officer.__str__(), officer.role)
