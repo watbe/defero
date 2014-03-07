55f32ce1ffabea98efef7ac872f4c87f633738d9
Wayne Tsai <wayne@tsai.co>
2014-03-07 14:43:21 +1100

    Removed useless commented out sections

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,11 +1,7 @@
 __author__ = 'Wayne'
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'defero.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
-
                        url(r'^user$', 'messenger.views.user_page'),
 
                        url(r'^logout$', 'messenger.views.user_logout'),
